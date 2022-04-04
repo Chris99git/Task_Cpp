c469f0c492baddd38e17736cb728e80a288db5a6
Chris99git <krzysztofligas7@gmail.com>
2022-04-04 16:45:45 +0200

    Create minki.cpp

---
+++ minki.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+int main() {
+    cout<< "/\"\"\"\\"<<endl;
+    cout<<"|o o|"<<endl;
+    cout<<"\\ - /\ "<<endl;
+    cout<<" |_|"<<endl;
+    cout<<"normalny"<<endl;
+
+    cout<< "\n/\"\"\"\\"<<endl;
+    cout<<"|O O|"<<endl;
+    cout<<"\\ U /\ "<<endl;
+    cout<<" |_|"<<endl;
+    cout<<"szczesliwy"<<endl;
+
+    cout<< "\n/\"\"\"\\"<<endl;
+    cout<<"|o o|"<<endl;
+    cout<<"\\ ~ /\ "<<endl;
+    cout<<" |_|"<<endl;
+    cout<<"zakochany"<<endl;
+
+    cout<< "\n '\`\'"<<endl;
+    cout<< "/\"\"\"\\"<<endl;
+    cout<<"|@ @|"<<endl;
+    cout<<"\\ = /\ "<<endl;
+    cout<<" |_|"<<endl;
+    cout<<"po sesji"<<endl;
+system("pause"); return 0;
+}
