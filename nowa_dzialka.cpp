7cdd703727a429fca206cfbca2659b7d6ed51e52
Chris99git <krzysztofligas7@gmail.com>
2022-04-04 22:15:24 +0200

    Create nowa_dzialka.cpp

---
+++ nowa_dzialka.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int ile;
+int x;
+
+int main()
+{
+    cin>>ile;
+    for(int i=1; i<=ile; i++)
+    {
+        cin>>x;
+        cout<<x*x<<endl;
+    }
+}
