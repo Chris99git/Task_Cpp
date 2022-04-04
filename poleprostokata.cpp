96e6efaf3f3b8f33f4ea68571ec678b9d1d78594
Chris99git <krzysztofligas7@gmail.com>
2022-04-04 16:45:47 +0200

    Create poleprostokata.cpp

---
+++ poleprostokata.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+int main() {
+float a,b;
+    cout<<"Program liczy pole prostokata \n";
+    cout<<"Podaj a\n";
+    cin>> a;
+    cout<<"Podaj b\n";
+    cin>> b;
+    cout<<"Pole prostokata wynosi "<<a*b <<endl;
+system("pause"); return 0;
+}
