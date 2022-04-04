9645de274170959e6f5ecf6fbe9d636b3a0ed6b4
Chris99git <krzysztofligas7@gmail.com>
2022-04-04 17:25:51 +0200

    Create trojkat_bud.cpp

---
+++ trojkat_bud.cpp
@@ -0,0 +1,12 @@
+//KRZYSZTOF LIGAS
+#include<iostream>
+using namespace std;
+int a,b,c;
+int main(){
+    cin>>a>>b>>c;
+    if(a+b>c && a+c>b && b+c>a)
+        cout<<"TAK"<<endl;
+    else
+        cout<<"NIE"<<endl;
+return 0;
+ }
