2556072c427b2eaedc320f117cae7eb53ad87341
Chris99git <krzysztofligas7@gmail.com>
2022-04-04 22:15:27 +0200

    Create mundial.cpp

---
+++ mundial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string napis="Mundial ";
+    char temp;
+    temp='S'- '!';
+    napis+=temp;
+    temp='m'-'=';
+    napis+=temp;
+    temp='n'-'=';
+    napis+=temp;
+    temp='T'-' ';
+    napis+=temp;
+    cout<<napis<<endl;
+}
