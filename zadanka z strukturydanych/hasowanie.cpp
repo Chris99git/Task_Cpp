2ddf0afa71b3520d8d57ca96c9a4ce4e0993eb5a
Chris99git <krzysztofligas7@gmail.com>
2022-04-05 18:52:05 +0200

    Create hasowanie.cpp

---
+++ zadanka z strukturydanych/hasowanie.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include<algorithm>
+using namespace std;
+
+const int m = 1000;
+bool U[m]; //slownik
+
+void Insert(unsigned int v)
+{
+	U[v] = true;
+}
+bool Search(unsigned int v)
+{
+	return U[v];
+//	if (U[v] == true)
+//		return true;
+//	else
+//		return false;
+}
+void Delete(unsigned int v)
+{
+	U[v] = false;
+}
+int main()
+{
+	Insert(5);
+	Insert(12);
+	cout << endl << Search(12);
+	cout << endl << Search(20);
+	Delete(5);
+	cout << endl << Search(5);
+}
